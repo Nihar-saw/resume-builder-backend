d3bb71872f3a4ceb86cc0c33967948ebfad2d19e
Nihar-sw <nihar11sawant@gmail.com>
2026-07-25 21:33:16 +0530

    increasing the timeout

--- src/config/ollama.js
+++ src/config/ollama.js
@@ -3,7 +3,7 @@ import { env } from "./env.js";
 
 const ollama = axios.create({
   baseURL: env.OLLAMA_URL || "http://localhost:11434",
-  timeout: 120000,
+  timeout: 300000,
 });
 
 export default ollama;
